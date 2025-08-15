903ab0468e384132ce952e0d4f4c9e14a1df8147
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-15 23:55:30 +0530

    chore: change home page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,15 +6,22 @@ export default function HomePage() {
     <div className="min-h-screen flex flex-col md:flex-row items-center justify-center bg-fixed bg-no-repeat bg-cover bg-center px-6 pt-16 pb-16 md:px-10 md:pt-24 md:pb-24 font-sans">
       <div className="flex flex-col md:flex-row items-center justify-center gap-6 md:gap-5">
         <div className="flex flex-col mt-10 items-center md:items-start text-center md:text-left">
-          <h4 className="font-poppins pl-[3px] text-1xl md:text-3xl">
+          <h4 className="font-poppins text-slate-800 pl-[3px] text-1xl md:text-3xl">
             Undergraduate at University of Moratuwa
           </h4>
-          <h1 className="font-black text-4xl md:text-7xl">Ishan Hansaka</h1>
+          <h1 className="font-black text-slate-800 text-4xl md:text-7xl">
+            Ishan Hansaka
+          </h1>
+          <div className="w-full md:w-[30rem] text-slate-600 text-base md:text-lg mt-6 md:text-left text-center">
+            I&apos;m a self-driven IT undergraduate passionate about software
+            engineering, quick to learn new technologies, and eager to take on
+            real-world challenges.
+          </div>
           <ViewResumeButton />
         </div>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="/images/ishan-dp.webp"
+          src="/images/ishan-dp2.webp"
           alt="Profile Picture"
           className="w-48 md:w-[25rem] rounded-full mb-10"
         />
