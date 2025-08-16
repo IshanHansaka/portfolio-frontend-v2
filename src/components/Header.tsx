7f4d74b2e262ad637f867a26c9d38a85aba9f444
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-16 13:36:10 +0530

    change: change styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="fixed top-0 left-0 z-20 h-auto w-full">
       <header>
         <div className="container-fluid mx-auto my-0 px-4 sm:px-6 md:px-7 lg:px-8 xl:px-8">
-          <div className="flex flex-col md:flex-row items-center justify-between">
+          <div className="flex items-center justify-between">
             <div className="flex-shrink-0 relative w-48 h-12 md:w-48 md:h-6 lg:w-60 lg:h-10 xl:w-80 xl:h-24">
               <Image
                 src="/images/name-logo3.webp"
