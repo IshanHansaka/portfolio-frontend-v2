5a53f99fe98910284bbffeaa71fadb9604925b28
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-17 23:24:20 +0530

    feat: create project module CRUD apis

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,35 @@
+import Project from '@/app/models/Project';
+import connectMongoDB from '@/app/lib/mongodb';
+import { NextResponse, NextRequest } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  try {
+    const project = await request.json();
+    await connectMongoDB();
+    const newProject = await Project.create(project);
+    return NextResponse.json(
+      { message: 'Project created successfully', project: newProject },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error('Error creating project:', error);
+    return NextResponse.json(
+      { message: 'Failed to create project' },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET() {
+  try {
+    await connectMongoDB();
+    const projects = await Project.find();
+    return NextResponse.json({ projects }, { status: 200 });
+  } catch (error) {
+    console.error('Error fetching projects:', error);
+    return NextResponse.json(
+      { message: 'Failed to fetch projects' },
+      { status: 500 }
+    );
+  }
+}
