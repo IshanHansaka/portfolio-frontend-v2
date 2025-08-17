6308f85a962d77dfe03a360f2b1a5711ddc55767
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-17 23:34:56 +0530

    chore: add env url to variable

--- src/service/blogService.ts
+++ src/service/blogService.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+
 export const fetchAllBlogs = async () => {
   try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/blogs`
-    );
+    const response = await axios.get(`${API_URL}/blogs`);
     console.log('Fetched blogs:', response.data);
     return response.data;
   } catch (error) {
--- src/service/projectService.ts
+++ src/service/projectService.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
+const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+
 export const fetchAllProjects = async () => {
   try {
-    const response = await axios.get(
-      `${process.env.NEXT_PUBLIC_BACKEND_API_URL}/projects`
-    );
+    const response = await axios.get(`${API_URL}/projects`);
     console.log('Fetched projects:', response.data);
     return response.data;
   } catch (error) {
