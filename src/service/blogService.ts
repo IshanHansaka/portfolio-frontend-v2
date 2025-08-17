10e1a07d025166790d0d7c80fb8eaa63e5bceeb3
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-17 23:53:19 +0530

    feat: replace backend url with nexjs inbuilt backend

--- src/service/blogService.ts
+++ src/service/blogService.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+// const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL;
+const path = '/api';
 
 export const fetchAllBlogs = async () => {
   try {
-    const response = await axios.get(`${API_URL}/blogs`);
+    // const response = await axios.get(`${API_URL}/blogs`);
+    const response = await axios.get(`${path}/blogs`);
     console.log('Fetched blogs:', response.data);
-    return response.data;
+    return response.data.blogs;
   } catch (error) {
     console.error('Error fetching blogs:', error);
     throw error;
