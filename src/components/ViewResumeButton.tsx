511884888f9176da8748f54beb636f88b42912b0
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-09-22 00:20:12 +0530

    chore: update Timeline2.jsx

--- src/components/ViewResumeButton.tsx
+++ src/components/ViewResumeButton.tsx
@@ -5,7 +5,7 @@ export default function ViewResumeButton() {
     <button
       onClick={() =>
         window.open(
-          'https://drive.google.com/file/d/1VGHA08VKCnMLp9YkO7OYfJ9jpK3_1rxY/view?usp=drive_link'
+          'https://drive.google.com/file/d/1CtvXqhEMZnVrst6paauZ30nny2iybmM3/view?usp=drive_link'
         )
       }
       className="mt-6 md:mt-6 px-6 py-2 font-medium cursor-pointer border-2 border-slate-800 bg-slate-800 text-white rounded-md hover:bg-white hover:text-slate-800 transition duration-300 ease-in-out"
