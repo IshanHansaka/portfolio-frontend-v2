d0d8dc5ff0087b987c961492db6be765cddcb69e
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2026-01-18 10:28:11 +0530

    chore: Change resume link in ViewResumeButton component
    Updated the URL in the ViewResumeButton component to point to a new Google Drive link.

--- src/components/ViewResumeButton.tsx
+++ src/components/ViewResumeButton.tsx
@@ -5,7 +5,7 @@ export default function ViewResumeButton() {
     <button
       onClick={() =>
         window.open(
-          'https://drive.google.com/file/d/1diFZynDbDSIYGWvP6UcPPfryF2Iw3JWX/view?usp=drive_link'
+          'https://drive.google.com/file/d/1ub7H7GdZu9JazcCmdh_vC4FuHCpFIhzL/view?usp=drive_link'
         )
       }
       className="mt-6 md:mt-6 px-6 py-2 font-medium cursor-pointer border-2 border-slate-800 bg-slate-800 text-white rounded-md hover:bg-white hover:text-slate-800 transition duration-300 ease-in-out"
